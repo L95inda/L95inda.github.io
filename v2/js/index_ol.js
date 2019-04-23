b0706ff5f553a62694eb38faa9da820cc6559197
L95inda <47975707+L95inda@users.noreply.github.com>
2019-04-23 11:46:44 +0100

    v2 - update

--- v2/js/index_ol.js
+++ v2/js/index_ol.js
@@ -351,30 +351,6 @@ $(document).ready(function () {
     $("audio").on("play", function () {
         StopAllAudio(this);
     });
-
-    // Check whether Internet Explorer is used and send message that it is not supported
-    function GetIEVersion() {
-        var sAgent = window.navigator.userAgent;
-        var Idx = sAgent.indexOf("MSIE");
-
-        // If IE, return version number.
-        if (Idx > 0)
-            return parseInt(sAgent.substring(Idx + 5, sAgent.indexOf(".", Idx)));
-
-        // If IE 11 then look for Updated user agent string.
-        else if (!!navigator.userAgent.match(/Trident\/7\./))
-            return 11;
-
-        else
-            return 0; //It is not IE
-    }
-    //alert(GetIEVersion());
-
-    if (GetIEVersion() > 0) {
-        alert("Internet Explorer is not supported. Please try again from another browser.");
-        $('#send').prop("disabled", true);
-    }
-    
     
 });
 
